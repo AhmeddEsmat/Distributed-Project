3bf64e9af6876d140040f8e970cf8d4c845759d9
Ibrahim Amr <ibrahimamr91@gmail.com>
2023-05-27 03:17:14 +0300

    updated server

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 from _thread import *
 import sys
 
-server = "192.168.1.11"
+server = "192.168.1.11"  # Enter the server IP address
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,15 +15,19 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
+
+
 def read_pos(str):
     str = str.split(",")
-    return int(str[0]), int(str[1])
+    return int(float(str[0])), int(float(str[1])), int(float(str[2]))
 
 
 def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
+    return str(tup[0]) + "," + str(tup[1]) + "," + str(tup[2])
 
-pos = [(0,0),(100,100)]
+
+pos = [(0,0,0),(100,100,0)]  # Update initial positions to include rotation angle
+rotation_angle = [0, 0]  # Track rotation angle for each player
 
 def threaded_client(conn, player):
     conn.send(str.encode(make_pos(pos[player])))
@@ -31,26 +35,28 @@ def threaded_client(conn, player):
     while True:
         try:
             data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            pos[player] = (data[0], data[1], rotation_angle[player])  # Update position and rotation angle
 
             if not data:
                 print("Disconnected")
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = make_pos(pos[0])
                 else:
-                    reply = pos[1]
+                    reply = make_pos(pos[1])
 
                 print("Received: ", data)
                 print("Sending : ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
-        except:
+            conn.sendall(str.encode(reply))
+        except Exception as e:
+            print("Error:", str(e))
             break
 
     print("Lost connection")
     conn.close()
+
 
 currentPlayer = 0
 while True:
